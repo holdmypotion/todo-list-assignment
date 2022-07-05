e6b60adfa61eacc9b073f94672b00c0af3e95b33
holdmypotion <holdmypotion@gmail.com>
2022-07-05 15:33:18 +0530

    for review

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+
+  resources :todo_lists do
+    resources :todo_items do
+      member do
+        patch :complete
+        patch :incomplete
+      end
+    end
+  end
+ 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Defines the root path route ("/")
+  root "todo_lists#index"
+end
